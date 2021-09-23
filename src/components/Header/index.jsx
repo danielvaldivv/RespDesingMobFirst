0d768dd1af2cf7339da9bca45548b6a2c2a82152
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-23 16:44:21 -0500

    [Changes] Header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import logo1 from '../../assets/imgs/logo1.svg'
+import arrowDown from '../../assets/icons/arrow_down.svg'
 
 import './Header.styl'
 
@@ -10,7 +11,7 @@ const Header = () =>  (
     <div className="header--title-container">
         <h1>La próxima revolución en el intercambio de criptomonedas.</h1>
         <p>Batabit te ayuda a navegar entre los diferentes precios y tendencias.</p>
-        <a href="#plans" className="header--button">Conoce Nuestros Planes <span /></a>
+        <a href="#plans" className="header--button">Conoce Nuestros Planes <img className="arrowDown" src={arrowDown} alt="ArrowDown" /></a>
     </div>
   </header>
 )
