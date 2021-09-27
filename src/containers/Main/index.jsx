bb9976c3d89f47cc95ceaf0d9bd482c4438fff86
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-27 09:33:37 -0500

    [Add] bitcoin img picture

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -4,15 +4,22 @@ import ExchangeContainer from '../../components/ExchangeContainer'
 import PlanContainer from '../../components/PlansContainer'
 import ProductDetail from '../../components/ProductDetail'
 
+import bitcoinGirl from'../../assets/imgs/fondo-bitcoin.jpg'
+
 import './Main.styl'
 
 const Main = () =>  (
     <main>
         <ExchangeContainer />
         <ProductDetail />
-        <section className="bitcoin-img-container">
+        <section className="picture">
+            <img
+                className="bitcoin-img-container"
+                src={bitcoinGirl}
+                alt="Girl"
+            />
             <h2>¡Conócelo hoy!</h2>
-        </section>
+        </ section>
         <PlanContainer/>
     </main>
   )
